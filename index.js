63a96d5f596b5d100e35c22cfd1888ab1ca3893e
Oscar <oscar.does.6@gmail.com>
2018-10-20 11:01:35 -0500

    :fire: Segundo commit con paquetes

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hola');
+// Variables de entorno. correr -> NOMBRE=oscar index.js
+// es un string totalmente
+// console.log(`Hola ${process.env.NOMBRE}`);
+
+// Variables de entorno. correr -> PORT=3000 index.js
+// console.log('Hola ' + process.env.PORT);
+
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello mundo');
+}).listen(port, hostname, () => {
+    console.log(`Corriendo en el puerto ${port}`);
+});
